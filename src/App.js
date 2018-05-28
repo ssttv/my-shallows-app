4ee04bc975c05f001f5ff6942b04b4a48213bd9e
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-05-28 16:58:51 +0300

    new App

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import './App.css';
 
 console.log(logo);
 
-
-function Img(props) {
-  return <img src={props.src} className={props.className} alt={props.alt} />;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
-function Movie(props) {
-  return (
-    <div className="movie-page">
-      <Img className="movie-img" src={props.imgSrc} alt={props.title} />
-      <h1 className="movie-title">{props.title}</h1>
-    </div>
-  );
-}
-
-const App = React.createElement(Movie, {
-  title: "2001: A Space Odyssey",
-  imgSrc: "https://i.imgur.com/vaZoNCA.jpg"
-});
-
-
-export default Movie;
+export default App;
